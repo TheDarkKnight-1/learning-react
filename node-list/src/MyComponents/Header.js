3a91dc5b27651773c62a95c1b642fea6aa25af84
TheDarkKnight-1 <chandanapmishra2017@gmail.com>
2021-05-28 17:33:17 +0000

    1st commit

---
+++ node-list/src/MyComponents/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
+export default function Header() {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">ToDosList</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-info">Search</Button>
+            </Form>
+        </Navbar>
+    )
+}
+
+
+
